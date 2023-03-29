c1796b88fdc52da10d345058421dac3bb94cda42
Mohamed Sami <mohamed.sami@intercom.com.eg>
2023-03-29 17:41:28 +0200

    Add discount page

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,17 +5,26 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { MenuComponent } from './menu/menu.component';
 import { DiscountComponent } from './discount/discount.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './discount/table/table.component';
+import { TapsComponent } from './discount/taps/taps.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
     MenuComponent,
-    DiscountComponent
+    DiscountComponent,
+    TableComponent,
+    TapsComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    AngularSvgIconModule.forRoot(),
+    NgbDropdownModule,
+    NgbNavModule
   ]
 })
 export class DashboardModule { }
---
+++ src/app/modules/dashboard/discount/taps/taps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prexie-taps',
+  templateUrl: './taps.component.html',
+  styleUrls: ['./taps.component.scss']
+})
+export class TapsComponent {
+  active = 1;
+}
